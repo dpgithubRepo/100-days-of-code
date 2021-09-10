24e1840b04431cdcea66e9f95a662195b831be26
Durga Prasad <durga.pras10@gmail.com>
2021-09-10 22:12:05 +0530

    comments added

--- soudhin/src/day12/TwoSum.java
+++ soudhin/src/day12/TwoSum.java
@@ -15,7 +15,7 @@ public class TwoSum {
 	
 	
 	public static boolean twoSumPairExisits(int[] arr, int k) {
-		Map<Integer, Integer> map = new HashMap<>();
+		Map<Integer, Integer> map = new HashMap<>();  // this method has extra space requirement for maintaining the map and can be further improved by 2 pointer technique
 		for(int i=0;i<arr.length;i++) {
 			int temp = arr[i];
 			if(map.containsKey(k-arr[i])){
