75fab0cffcb9a38fb15ac0c98dc149562ede6e63
Durga Prasad <durga.pras10@gmail.com>
2021-09-13 18:59:40 +0530

    added the comments & bit operation explanations

--- soudhin/src/day15/CheckKBitSet.java
+++ soudhin/src/day15/CheckKBitSet.java
@@ -4,6 +4,15 @@ package day15;
 /**
  * Given a decimal number and kth bit check if the kth bit is set(=1) or not
  * @author Durga Prasad
+ * 
+ * binary of 5 ->   0101
+ * 
+ * binary of 1 -->  0001
+ * 
+ * (k-1) that is 3-1 = 2 left shift of 1 => 0100  (only kth bit will be 1 and others 0)
+ * 
+ * 5 & (k-1) shift of 5   0101 & 0100  the result will be 0 if kth bit of 5 is 0 else 1
+ * 
  *
  */
 public class CheckKBitSet {
