80af520b1518033a7cb563e078db372d83f04434
Durga Prasad <durga.pras10@gmail.com>
2021-09-25 12:25:42 +0530

    pattern 10 fix

--- soudhin/src/day22/Pattern10.java
+++ soudhin/src/day22/Pattern10.java
@@ -6,41 +6,39 @@ public class Pattern10 {
 	public static void main(String[] args) {
 		System.out.println("Enter the number");
 		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		scanner.close();
-		
-		int _innerSpace = n/2;
-		int _outerSpace = 0;
-		
-		for(int i = 1; i<=n ; i++) {
-			
-			for(int j = 1; j <= _innerSpace; j++) {
+				int n = scanner.nextInt();
+				scanner.close();
 				
-				System.out.print("\t");
-			}
-			
-			System.out.print("*\t");
-			
-			for(int j = 1; j <= _outerSpace; j++) {
+				int _innerSpace = n/2+1;
+				int _outerSpace = 0;
 				
-				System.out.print("\t");
-			}
-			
-			System.out.print("*\t");
-			
-			if( i <= n/2) {
-				
-				_innerSpace --;
-				_outerSpace += 2;
-				
-			}else {
-				
-				_innerSpace ++;
-				_outerSpace -= 2;
-				
-			}
-			
-			System.out.println();
-		}
+				for(int i = 1; i<=n ; i++) {
+					
+					for(int j = 1; j <= _innerSpace; j++) {
+							System.out.print("\t");
+					}
+					
+					System.out.print("*");
+					
+					for(int j = 1; j <= _outerSpace; j++) {
+						System.out.print("\t");
+				}
+					
+					if(i!=1 && i != n)
+						System.out.print("*");
+					
+					System.out.println();
+					
+					if(i <= n/2) {
+						_innerSpace--;
+						_outerSpace += 2;
+					}else {
+						_innerSpace++;
+						_outerSpace -= 2;
+					}
+					
+					
+					
+				}
 	}
 }
