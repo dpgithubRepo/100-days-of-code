54afcc0c7c1d2c435b4b4f7524941d69137e9808
durga prasad <durga.pras10@gmail.com>
2021-10-03 23:02:53 +0530

    pattern 15 & 16

--- soudhin/src/day31/PatternsPractice.java
+++ soudhin/src/day31/PatternsPractice.java
@@ -1,37 +1,65 @@
 package day31;
+
 import java.util.Scanner;
 
 public class PatternsPractice {
-	
+
 	public static void pattern15(int n) {
-		int space = n/2;
+		int space = n / 2;
 		int star = 1;
 		int val = 1;
-		for(int i = 1; i <= n; i++) {
+		for (int i = 1; i <= n; i++) {
 			int col = val;
-			for(int j = 1; j <= space; j++) {
+			for (int j = 1; j <= space; j++) {
 				System.out.print("\t");
 			}
-			for(int j = 1; j <= star; j++) {
-				System.out.print(col +"\t");
-				if(j <= star/2)
+			for (int j = 1; j <= star; j++) {
+				System.out.print(col + "\t");
+				if (j <= star / 2)
 					col++;
-				else 
+				else
 					col--;
-				
+
 			}
-			
-			if(i <= n/2) {
-				star+=2;
+
+			if (i <= n / 2) {
+				star += 2;
 				space--;
 				val++;
-			}else {
-				star-=2;
+			} else {
+				star -= 2;
 				space++;
 				val--;
 			}
-		
-		System.out.println();
+
+			System.out.println();
+		}
+
+	}
+
+	public static void pattern16(int n) {
+		int cols = 2 * n - 1;
+		for (int i = 1; i <= n; i++) {
+			int val = 1;
+			for (int j = 1; j <= cols / 2 + 1; j++) {
+				if (j <= i) {
+					System.out.print(val++ + "\t");
+				} else {
+					System.out.print("\t");
+				}
+			}
+
+			if (i == n)
+				val--;
+			for (int k = cols / 2 + 2; k <= cols; k++) {
+				if (k <= cols - i) {
+					System.out.print("\t");
+				} else {
+					System.out.print(--val + "\t");
+				}
+
+			}
+			System.out.println();
 		}
 	}
 
@@ -39,8 +67,7 @@ public class PatternsPractice {
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
 		scanner.close();
-		
-		
-		}
-		
+
+	}
+
 }
