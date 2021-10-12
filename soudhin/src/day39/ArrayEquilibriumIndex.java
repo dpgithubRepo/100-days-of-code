f0c2790cc970fa6bf10bbf003692fb2a1ba9cedb
unknown <durga.pras10@gmail.com>
2021-10-12 22:08:38 +0530

    find equilibrium index of an array

---
+++ soudhin/src/day39/ArrayEquilibriumIndex.java
@@ -0,0 +1,61 @@
+package day39;
+
+public class ArrayEquilibriumIndex {
+
+	public static int getEquilibriumIndexBrutForce(int [] arr) {
+		int index = -1;
+		int len = arr.length;
+		
+		for(int i = 0; i < len; i++) {
+			int ls = 0;
+			int rs = 0;
+			for(int j = 0; j < i; j++) {
+				ls +=  arr[j];
+			}
+			
+			for(int k = i+1; k < len; k++) {
+				rs += arr[k];
+			}
+			
+			if(ls == rs) return i;
+		}
+		
+		return index;
+	}
+	
+	public static int getEquilibriumIndexSlidingWindow(int [] arr) {
+		int index = -1;
+		int len = arr.length;
+		int sum = 0;
+		int rs = 0;
+		int ls = 0;
+		for(int i = 1; i < len; i++) {
+			sum += arr[i];
+		}
+		
+		for(int j = 1; j < len; j++) {
+			ls += arr[j-1];
+			rs = sum -  arr[j];
+			
+			if(ls == rs) return j;
+		}
+		
+		return index;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("*** Following is Brut Force approach ***");
+		System.out.println(getEquilibriumIndexBrutForce(new int[] {2,1,2}));
+		System.out.println(getEquilibriumIndexBrutForce(new int[] {1,2,3}));
+		System.out.println(getEquilibriumIndexBrutForce(new int[] {12,0,2,3,9}));
+		System.out.println(getEquilibriumIndexBrutForce(new int[] {10,10,10,10,10,0,50}));
+		
+		
+		System.out.println("*** Following is sliding window approach ***");
+		System.out.println(getEquilibriumIndexSlidingWindow(new int[] {2,1,2}));
+		System.out.println(getEquilibriumIndexSlidingWindow(new int[] {1,2,3}));
+		System.out.println(getEquilibriumIndexSlidingWindow(new int[] {12,0,2,3,9}));
+		System.out.println(getEquilibriumIndexBrutForce(new int[] {10,10,10,10,10,0,50}));
+	}
+}
