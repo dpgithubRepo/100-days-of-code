4f960afaabe27fc0382debc85543243eeab5786d
unknown <durga.pras10@gmail.com>
2021-10-15 21:54:30 +0530

    made rotation generic to postivei, negative & higher values

--- soudhin/src/day40/RotateNumber.java
+++ soudhin/src/day40/RotateNumber.java
@@ -3,19 +3,32 @@ package day40;
 import java.util.Scanner;
 /**
  * 
- * Given a positive integer n  and a number to rotate by, rotate the n by the rotate number
+ * 1. You are given two numbers n and k. You are required to rotate n, k times to the right. If k is positive, rotate to the right i.e. remove rightmost digit and 
+ * make it leftmost. Do the reverse for negative value of k. 
+ * Also k can have an absolute value larger than number of digits in n
  * 
- * eg : n = 12345678 rotate by = 2
- * o/p = 78123456
  * 
+ * 2. Take as input n and k.
  *  
- * n = 54321 rotate by = 1
- * o/p = 15432
+ * 3. Print the rotated number.
+ *  
+ *  
+ * example 1 :
+ *  
+ * n =  56789123
+ * k = 3
+ * output =  12356789
  * 
  * 
- * n = 23456 rotate by = 0
- * o/p = 23456
- * 
+ * n =  56789123
+ * k = 0
+ * output =  56789123
+ *  
+ *  
+ * n =  56789123
+ * k = -2
+ * output = 78912356
+ *  
  * @author Durga Prasad
  *
  */
@@ -23,26 +36,38 @@ public class RotateNumber {
 public static void main(String[] args) {
 	Scanner scanner = new Scanner(System.in);
 	int n = scanner.nextInt();
-	int temp = n;
-	int rotateBy = scanner.nextInt();
+	int k = scanner.nextInt();
 	scanner.close();
-	int pow = 1;
-
-	while(rotateBy > 0) {
-		pow *= 10;
-		rotateBy--;
+	
+	int temp = n;
+	int div = 1;
+	int mul = 1;
+	int nod = 0;
+	
+	 while(temp > 0) {
+		nod++;
 		temp = temp/10;
 	}
-	
-	int prefix = n % pow;
-	 pow = 1;
-	 int temp1 = temp;
-	 while(temp1 > 0) {
-		 pow *= 10;
-		 temp1 /= 10;
+	 
+	 k = k % nod;
+	 
+	 if(k < 0) {
+		 k = k + nod;
+	 }
+	 
+	 for(int i = 1; i <= nod; i++) {
+		 if(i <= k) {
+			 div *= 10;
+		 }else {
+			 mul *= 10;
+		 }
 	 }
 	
-	 System.out.println(prefix*pow +temp );
+	 int q = n / div;
+	 
+	 int r = n % div;
+	 
+	 System.out.println(r * mul + q);
 	
 }
 }
